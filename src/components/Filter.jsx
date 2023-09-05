e82bb60384f3ffe452b49256c97b5df3ae58052e
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2023-09-04 21:52:56 -0300

    loading compo

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,7 +3,7 @@ import {
   Card, 
   Col, 
   Container, 
-  Dropdown, DropdownButton, FormControl, Row } from "react-bootstrap";
+  Dropdown, DropdownButton, FormControl, Row, Spinner } from "react-bootstrap";
 
 export default function Filter() {
 
@@ -14,16 +14,20 @@ export default function Filter() {
 
   
   useEffect(() => {
-    const getProducts = () => {
-      const URL = 'https://crud-backend-pagination.onrender.com/api/all-products'
-      fetch(URL).then(res=> res.json()).then(data=>{ 
-        setProducts(data)
-
-        const uniqueCategories = [...new Set(data.map(product => product.category))];
-        setCategories(uniqueCategories)
-      })
+    try {
+      const getProducts = () => {
+        const URL = 'https://crud-backend-pagination.onrender.com/api/all-products'
+        fetch(URL).then(res=> res.json()).then(data=>{ 
+          setProducts(data)
+  
+          const uniqueCategories = [...new Set(data.map(product => product.category))];
+          setCategories(uniqueCategories)
+        })
+      }
+      getProducts()
+    } catch (error) {
+      console.log(error);
     }
-    getProducts()
   },[])
 
   const searcher = (e) => {
@@ -74,22 +78,33 @@ export default function Filter() {
         </Col>
       </Row>
     </Container>
-    <Container>
-      <Row>
-        {filteredProducts.map((product) => (
-          <Col className="mb-4" xs={6} md={4} lg={3} key={product._id}>
-            <Card className='filtered-card' >
-              <Card.Img variant="top" src={product.cover} />
-              <Card.Body>
-                <h6>{product.name}</h6>
-                <Card.Text><i>Category: </i>{product.category}</Card.Text>
-                <Card.Text><i>Price: </i>{product.price}</Card.Text>
-              </Card.Body>
-              </Card>
-        </Col>
-        ) )}
-      </Row>
-    </Container>
+    {filteredProducts.length > 0 ? (
+        <Container>
+          <Row>
+            {filteredProducts.map((product) => (
+              <Col className="mb-4" xs={6} md={4} lg={3} key={product._id}>
+                <Card className='filtered-card' >
+                  <Card.Img variant="top" src={product.cover} />
+                  <Card.Body>
+                    <h6>{product.name}</h6>
+                    <Card.Text><i>Category: </i>{product.category}</Card.Text>
+                    <Card.Text><i>Price: </i>{product.price}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      ) : (
+        <Container style={{ textAlign: 'center' }}>
+          <p >Loading products...</p>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading products...</span>
+          </Spinner>
+        </Container>
+      )}
     </>
   );
 }
+
+
